305713b7c26f8073d2f02eea0f3463184fe71f5f
Alexander Mireles <alexandermireles0@gmail.com>
2015-05-13 16:12:09 -0700

    initial commit

---
+++ js/tictacController.js
@@ -0,0 +1,13 @@
+angular
+    .module('tictacApp')
+    .controller('TictacController', TictacController);
+
+
+    TictacController.$inject = ['$firebaseObject'];
+
+
+    function TictacController($firebaseObject){
+        var self = this;
+        
+
+    }
