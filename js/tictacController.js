97b02f6720c00d188b9f82f54ab865fc21ebd68d
Alexander Mireles <alexandermireles0@gmail.com>
2015-05-17 18:51:36 -0700

    prefinal save

--- js/tictacController.js
+++ js/tictacController.js
@@ -1,77 +1,66 @@
 angular
-    .module('tictacApp')
-    .controller('TictacController', TictacController);
+   .module('tictacApp')
+   .controller('TictacController', TictacController);
 
 
-    TictacController.$inject = ['$firebaseObject'];
-        // so these functions are set up and running angularfire and are the basic functions of the tictac toe board. Still need to introduce functions for the two player version.
+   TictacController.$inject = ['$firebaseObject'];
+       // so these functions are set up and running angularfire and are the basic functions of the tictac toe board. Still need to introduce functions for the two player version.
 
-    function TictacController($firebaseObject, $index){ //constructor functin that builds cntrls my angular app
-        var self = this; //reassigning keyword this to a variable called self (cos john papa says so) 
-        
-        self.board = getBoard(); //retrieves board from firebase and attaches to controller
-        createBoxes(); // 
-        self.clickYou = clickYou;
+   function TictacController($firebaseObject, $index){ //constructor functin that builds cntrls my angular app
+       var self = this; //reassigning keyword this to a variable called self (cos john papa says so) 
+       
+       self.board = getBoard(); //retrieves board from firebase and attaches to controller
+       createBoxes(); // 
+       self.clickYou = clickYou;
+       self.playerX = true;
+       self.playerO = false;
+       
+   
+       function getBoard(){
+           // counter++;
+           // console.log(counter);
+           var ref = new Firebase("https://ttt3.firebaseio.com/");
+           var board = $firebaseObject(ref);
+           return board;
+           // Retrieves board from firebase
+       }
 
-        // self.player1 ="x";
-        // self.player2 ="o";
-        // self.turn ="o";
+       function createBoxes(){
+           // console.log("running createBoxes")
+           // console.log(self.board)
+           self.board.boxes = [{active: false, p1owns:"" }, {active: false, p1owns:""}, {active: false, p1owns:""}, {active: false, p1owns:""}, {active: false, p1owns:""}, {active: false, p1owns:""}, {active: false, p1owns:""}, {active: false, p1owns:""}, {active: false, p1owns:""}]; 
+           self.board.counter = 0; // our game counter
+           self.board.$save();  
+           console.log(self.board.counter)
+       }
 
-        
-        // self.restart = restart;
-        // self.getWinner = getWinner;
-        
-    
-        function getBoard(){
-            // counter++;
-            // console.log(counter);
-            var ref = new Firebase("https://ttt3.firebaseio.com/");
-            var board = $firebaseObject(ref);
-            return board;
-        }
-
-        function createBoxes(){
-            // console.log("running createBoxes")
-            // console.log(self.board)
-            self.board.boxes = [{active: false, p1owns:false }, {active: false}, {active: false}, {active: false}, {active: false}, {active: false}, {active: false}, {active: false}, {active: false}]; 
-            self.board.counter = 0; // our game counter
-            self.board.$save();  
-            console.log(self.board.counter)
-        }
-        
-        
-        
-        function clickYou($index){
-            if (self.board.boxes[$index].active == true) {
-                alert("seat's taken")
-            } else {
-                self.board.boxes[$index].active = true;
-                self.board.counter +=1;
-                console.log(self.board.counter);
-                self.board.$save();
-                
+       function clickYou($index){
+           if (self.board.boxes[$index].p1owns != "") {
+               alert("seat's taken")
+               
             }
 
+            else {
 
+               if (self.playerX == true) {
+                   self.board.boxes[$index].active[$index]= true;
+                   self.board.boxes[$index].p1owns = "X";
+                   self.board.counter +=1;
+                   console.log(self.board.counter);
+                   self.board.$save(); 
+                   self.playerX = false ;   
+               }
+               else {
+                   self.board.boxes[$index].active[$index] = false;
+                   self.board.boxes[$index].p1owns = "O";
+                   self.board.counter +=1;
+                   console.log(self.board.counter);
+                   self.board.$save(); 
+                   self.playerX = true ; 
 
-        //      function clickedBox(c) {
-        // if (self.addBox[c].box == '' && self.turn == "o") {
-        //  self.addBox[c].box = "x";
-        //  self.turn = "x";
-        //  self.addBox.$save(c);
-        // }    else if (self.addBox[c].box == '' && self.turn == "x") {
-        // self.addBox[c].box = "o";
-        // self.turn = "o";
-        // self.addBox.$save(c);
-        // }      else {
-        // null
+                   function 
+               } 
+            }
+       }
 
-        
-
-
-
-            
-         }
-
-    }
-
+   }
